6ba3a013bc0e432e754c8027c497eb63d0651d25
Arttu Kuikka <arttu.juhani.kuikka@gmail.com>
2022-10-21 19:41:19 +0300

    testi

--- Controllers/RuokalistaAPIController.cs
+++ Controllers/RuokalistaAPIController.cs
@@ -207,7 +207,7 @@ namespace RuokalistaServer.Controllers
                 {
                     if (!RuokalistaExists(ruokalista.Id))
                     {
-                        return NotFound();
+                        return NotFound("Doesn't exist in db");
                     }
                     else
                     {
