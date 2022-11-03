2b65dbf41a475b63848069bb10e9b40217e934a8
Arttu Kuikka <75498768+ArttuKuikka@users.noreply.github.com>
2022-11-03 23:09:30 +0200

    jotain

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,35 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
+var $el = $("#very-specific-design");
+var elHeight = $el.outerHeight();
+var elWidth = $el.outerWidth();
 
+var $wrapper = $("#scaleable-wrapper");
+
+$wrapper.resizable({
+    resize: doResize
+});
+
+function doResize(event, ui) {
+
+    var scale, origin;
+
+    scale = Math.min(
+        ui.size.width / elWidth,
+        ui.size.height / elHeight
+    );
+
+    $el.css({
+        transform: "translate(-50%, -50%) " + "scale(" + scale + ")"
+    });
+
+}
+
+var starterData = {
+    size: {
+        width: $wrapper.width(),
+        height: $wrapper.height()
+    }
+}
+doResize(null, starterData);
 // Write your JavaScript code.
