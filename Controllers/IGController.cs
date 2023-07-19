17ac3c8ad8a177c99429f257557c3d5b78ebcfaf
Arttu Kuikka <75498768+ArttuKuikka@users.noreply.github.com>
2023-07-19 22:09:21 +0300

    Päivän haku oikein

--- Controllers/IGController.cs
+++ Controllers/IGController.cs
@@ -23,7 +23,7 @@ namespace RuokalistaServer.Controllers
             var viikonekapaiva = GetFirstDayOfWeek(vuosi, viikko);
             var viikonperjantai = viikonekapaiva.AddDays(4);
 
-            ViewBag.Paivat = viikonekapaiva.ToString("d.MM") + "-" + viikonperjantai.ToString("d.MM");
+            ViewBag.Paivat = viikonekapaiva.ToString("dd.MM") + "-" + viikonperjantai.ToString("dd.MM");
 
 
 			if (weekId == null)
@@ -70,19 +70,32 @@ namespace RuokalistaServer.Controllers
             return View();
         }
 
+		
 		public static DateTime GetFirstDayOfWeek(int year, int weekOfYear)
 		{
 			DateTime jan1 = new DateTime(year, 1, 1);
-			int daysOffset = DayOfWeek.Monday - jan1.DayOfWeek;
-			DateTime firstMonday = jan1.AddDays(daysOffset);
-			int firstWeek = CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(jan1, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+			int daysOffset = DayOfWeek.Thursday - jan1.DayOfWeek;
 
-			if (firstWeek <= 1)
+			// Use first Thursday in January to get first week of the year as
+			// it will never be in Week 52/53
+			DateTime firstThursday = jan1.AddDays(daysOffset);
+			var cal = CultureInfo.CurrentCulture.Calendar;
+			int firstWeek = cal.GetWeekOfYear(firstThursday, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+
+			var weekNum = weekOfYear;
+			// As we're adding days to a date in Week 1,
+			// we need to subtract 1 in order to get the right date for week #1
+			if (firstWeek == 1)
 			{
-				weekOfYear -= 1;
+				weekNum -= 1;
 			}
 
-			return firstMonday.AddDays(weekOfYear * 7);
+			// Using the first Thursday as starting week ensures that we are starting in the right year
+			// then we add number of weeks multiplied with days
+			var result = firstThursday.AddDays(weekNum * 7);
+
+			// Subtract 3 days from Thursday to get Monday, which is the first weekday in ISO8601
+			return result.AddDays(-3);
 		}
 
 	}
