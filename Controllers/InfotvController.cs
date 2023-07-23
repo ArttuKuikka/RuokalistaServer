4157303af78b0768df45103f003b2d37e1471752
Arttu Kuikka <75498768+ArttuKuikka@users.noreply.github.com>
2023-07-23 23:19:17 +0300

    ruokalista v2

--- Controllers/InfotvController.cs
+++ Controllers/InfotvController.cs
@@ -54,5 +54,47 @@ namespace RuokalistaServer.Controllers
             ViewBag.ruokalista = ruokalista;
             return View();
         }
+
+        public IActionResult V2()
+        {
+			ViewBag.Nykyinenviikko = false;
+			ViewBag.RuokaOlemassa = false;
+			var viikko = System.Globalization.ISOWeek.GetWeekOfYear(DateTime.Now);
+			var vuosi = DateTime.Now.Year;
+			if (DateTime.Today.DayOfWeek == DayOfWeek.Sunday || DateTime.Today.DayOfWeek == DayOfWeek.Saturday)
+			{
+				ViewBag.NytOnSeuraavaViikko = true;
+				viikko += 1;
+			}
+			if (DateTime.Today.DayOfWeek == DayOfWeek.Friday && DateTime.Now.Hour > 12)
+			{
+				if (db.Ruokalista.Where(m => m.Year == DateTime.Now.Year)?.FirstOrDefault(k => k.WeekId == viikko + 1) != null)
+				{
+					ViewBag.NytOnSeuraavaViikko = true;
+					viikko += 1;
+				}
+			}
+
+			ViewBag.viikko = viikko;
+
+			var ruokalista = db.Ruokalista.Where(m => m.Year == vuosi)?.FirstOrDefault(k => k.WeekId == viikko);
+
+
+			if (ruokalista != null)
+			{
+				if (ruokalista.WeekId == System.Globalization.ISOWeek.GetWeekOfYear(DateTime.Now))
+				{
+					ViewBag.Nykyinenviikko = true;
+
+				}
+				ViewBag.RuokaOlemassa = true;
+			}
+
+
+			ViewBag.SeuraavaViikkoNumero = viikko + 1;
+
+			ViewBag.ruokalista = ruokalista;
+			return View();
+		}
     }
 }
