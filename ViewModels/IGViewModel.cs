ed85ce1c0edb1e56511a9946e9736867d6d9e684
Arttu Kuikka <75498768+ArttuKuikka@users.noreply.github.com>
2024-10-04 22:40:10 +0300

    IG model

---
+++ ViewModels/IGViewModel.cs
@@ -0,0 +1,10 @@
+﻿using RuokalistaServer.Models;
+
+namespace RuokalistaServer.ViewModels
+{
+    public class IGViewModel
+    {
+        public Ruokalista Ruokalista { get; set; }
+        public string Päivät { get; set; }
+    }
+}
