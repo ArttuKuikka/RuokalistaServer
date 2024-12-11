8a2e07e2acafc06cecbf2aa2fdfcee7218713921
Arttu Kuikka <75498768+ArttuKuikka@users.noreply.github.com>
2024-12-11 19:02:26 +0200

    fix model

--- src/Models/KasvisRuokalista.cs
+++ src/Models/KasvisRuokalista.cs
@@ -8,9 +8,9 @@ namespace RuokalistaServer.Models
     {
         [JsonIgnore]
         public int Id { get; set; }
-		[Range(1, 54, ErrorMessage = "Viikko ei voi olla pienempi kuin 1 tai suurempi kuin 54")]
+		
 		public int WeekId { get; set; }
-		[Range(2020, 2100, ErrorMessage = "Virheellinen vuosiluku")]
+		
 		public int Year { get; set; }
 		[MaxLength(150)]
 		public string? Maanantai { get; set; }
