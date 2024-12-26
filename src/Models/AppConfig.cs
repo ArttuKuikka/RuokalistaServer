8e722df56c631179ff6d8f806e15e6057912eaa7
Arttu Kuikka <75498768+ArttuKuikka@users.noreply.github.com>
2024-12-26 13:37:20 +0200

    App config

---
+++ src/Models/AppConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace RuokalistaServer.Models
+{
+	public class AppConfig
+	{
+		public string Branding { get; set; }
+		public string PrimaryColor { get; set; }
+		public bool KasvisruokalistaEnabled { get; set; }
+		public bool AanestysEnabled { get; set; }
+		public string DefaultLanguage { get; set; }
+
+	}
+}
